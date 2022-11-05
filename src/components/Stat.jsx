288594a50dc919d17198d82b2d10baea98ee9d47
Alisha <alishamohasin85@gmail.com>
2022-11-05 17:58:30 +0530

    updated

--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,10 +1,11 @@
 import GitHubCalendar from 'react-github-calendar';
 import "./style/Stat.scss"
 
+
 const Stat=()=>{
     return(
         <div className='calender'>
-            <GitHubCalendar username="AlishaMohasin"  />
+            <GitHubCalendar username="AlishaMohasin"   />
        </div>
 
     )
